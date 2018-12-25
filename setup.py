b9f8d27fa1a0e59ede83a982185e3e48f1127c72
Paul Roeland <paul@cleanclothes.org>
2018-12-25 15:42:13 +0100

    "Add theme: RFD 2019 Theme"

--- setup.py
+++ setup.py
@@ -40,6 +40,8 @@ setup(
     zip_safe=False,
     install_requires=[
         # -*- Extra requirements: -*-
+        'collective.themefragments',
+        'collective.themesitesetup',
         'plone.api>=1.8.4',
         'Products.GenericSetup>=1.8.2',
         'setuptools',
